corrected SMF calculation

SMF Analysis/leastsq_fitting.py
@@ -8,6 +8,8 @@ Created on Wed Nov  3 15:55:20 2021
 import numpy as np
 from scipy.optimize import minimize, least_squares
 
+import pdb
+
 ## MAIN FUNCTION
 def lsq_fit(smf, hmf, smf_model, z = 0):
     '''
@@ -24,6 +26,8 @@ def lsq_fit(smf, hmf, smf_model, z = 0):
     
     # create data for modelled smf (for plotting)
     m_star_range = np.logspace(6,12,1000)
+    #pdb.set_trace()
+    #print(par)
     modelled_phi = smf_model.function(m_star_range, par)
     modelled_smf = np.array([m_star_range, modelled_phi]).T
     return(par, modelled_smf, cost)
@@ -41,7 +45,7 @@ def cost_function(params, smf, smf_model):
     
     phi_mod = smf_model.function(m_obs, params)
     
-    if not within_bounds(params, [0,0,0], [np.inf,np.inf,np.inf]):
+    if not within_bounds(params, [0,0,0], [1,np.inf,np.inf]):
         return(1e+10) # return inf (or huge value) if outside of bounds
 
     res = np.log10(phi_obs) - np.log10(phi_mod)
@@ -51,12 +55,12 @@ def cost_function(params, smf, smf_model):
 ## HELP FUNCTIONS
 def within_bounds(values, lower_bounds, upper_bounds):
     '''
-    Checks if list of values is within lower and upper bounds (if value is equal
-    to bound, also return true). All three arrays must have same length.
+    Checks if list of values is within lower and upper bounds (strictly
+    within bounds). All three arrays must have same length.
     '''
     is_within = []
     for i in range(len(values)):
-        is_within.append((values[i] >= lower_bounds[i]) & (values[i] <= upper_bounds[i]))
+        is_within.append((values[i] > lower_bounds[i]) & (values[i] < upper_bounds[i]))
         
     # return True if all elements are within bounds, otherwise False
     if all(is_within)==True:

SMF Analysis/smf_modelling_saturday_try.py
@@ -7,6 +7,8 @@ Created on Fri Nov 19 21:34:38 2021
 
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy.optimize import root_scalar
+from scipy.special import hyp2f1
 import leastsq_fitting
 import mcmc_fitting
 
@@ -63,18 +65,20 @@ class smf_model_class():
         self.hmf_function   = interp1d(*hmf.T) # turn hmf data into evaluable function (using linear interpolation)
         self.feedback_model = feedback_model(feedback_name, m_crit) # choose feedback model function
         
-    def function(self, m, params):
+    def function(self, m_star, params):
         '''
-        Create SMF model function by multiplying HMF function with feedback model function.
+        Create SMF model function by multiplying HMF function with feedback model 
+        derivative.
         '''
-        print(params)
-        print(m)
         
         # check that parameters are sensible, otherwise invert function will
         # fail to determine halo masses
         if np.any(params<0) or (params[0]==0):
             return(1e+10) # basically makes cost func infinite
-        return(self.hmf_function(self.feedback_model.function(m, *params)))
+        
+        # calculate halo masses from stellar masses using model
+        m_h = self.feedback_model.calculate_m_h(m_star, *params) 
+        return(self.hmf_function(m_h) * self.feedback_model.calculate_dlogmh_dlogmstar(m_h,*params))
 
 # DEFINE THE FEEDBACK MODELS
 def feedback_model(feedback_name, m_crit):
@@ -92,33 +96,72 @@ def feedback_model(feedback_name, m_crit):
     if feedback_name == 'sn':  
         model = supernova_feedback(feedback_name, m_crit)
     if feedback_name == 'both':  
-        model = supernova_blackhole_feedvack(feedback_name, m_crit)
+        model = supernova_blackhole_feedback(feedback_name, m_crit)
     return(model)
 
+# the feedback models with all necessary parameter and functional equations
+# see overleaf notes where these equations come from
 class no_feedback():
     def __init__(self, feedback_name, m_crit):
         self.name          = feedback_name
         self.m_c           = m_crit
-        self.initial_guess = [100]
-    def function(self, m, A):
-        return(A*m)
+        self.initial_guess = [0.01]
+    def calculate_m_star(self, m_h, A):
+        return(A*m_h)
+    def calculate_m_h(self, m_star, A):
+        return(m_star/A)
+    def calculate_dlogmh_dlogmstar(self, m_h, A):
+        return(1)        
     
 class supernova_feedback():
     def __init__(self, feedback_name, m_crit):
         self.name          = feedback_name
         self.m_c           = m_crit
-        self.initial_guess = [100, 1] 
-    def function(self, m, A, alpha):
-        return( A * (m/self.m_c)**alpha *m)    
+        self.initial_guess = [0.01, 1] 
+    def calculate_m_star(self, m_h, A, alpha):
+        return( A/(alpha+1) * (m_h/self.m_c)**alpha * m_h)
+    def calculate_m_h(self, m_star, A, alpha):
+        return((m_star* (alpha+1)/A* self.m_c**alpha)**(1/(alpha+1)))
+    def calculate_dlogmh_dlogmstar(self, m_h, A, alpha):
+        return(1/(alpha+1))
 
-class supernova_blackhole_feedvack():
+class supernova_blackhole_feedback():
     def __init__(self, feedback_name, m_crit):
         self.name          = feedback_name
         self.m_c           = m_crit
-        self.initial_guess = [100, 1, 1]
-    def function(self, m, A, alpha, beta):
-        return(A * 1/((m/self.m_c)**(-alpha)+(m/self.m_c)**(beta)) *m)        
+        self.initial_guess = [0.01, 1, 0.3]
+    def calculate_m_star(self, m_h, A, alpha, beta):
+        eta = (alpha+1)/(alpha+beta)
+        alphabet = alpha+beta
+        x = (m_h/self.m_c)
+        return(A/(alpha+1) * self.m_c * x**(1+alpha) * hyp2f1(1, eta, eta+1, -x**alphabet))
+    def calculate_m_h(self, m_star, A, alpha, beta):
+        m_star_func = lambda m_halo : self.calculate_m_star(m_halo, A, alpha, beta)
+        gradient    = lambda m_halo : self._calculate_dmstar_dmh(m_halo, A, alpha, beta)
+        m_h = invert_function(m_star_func, gradient, m_star) 
+        return(m_h)
+    def calculate_dlogmh_dlogmstar(self, m_h, A, alpha, beta):
+        eta = (alpha+1)/(alpha+beta)
+        alphabet = alpha+beta
+        x = (m_h/self.m_c)
+        return((1+x**alphabet)/(1+alpha)*hyp2f1(1, eta, eta+1, -x**alphabet))
+    def _calculate_dmstar_dmh(self, m_h,A,alpha,beta): # just used to calc inverse
+        return(A * 1/((m_h/self.m_c)**(-alpha)+(m_h/self.m_c)**(beta)))
 
+
+        
+## HELP FUNCTIONS
+def invert_function(func, fprime, y):
+    '''
+    For a function y=f(x), calculate x values for an input set of y values.
+
+    '''
+    x = []
+    
+    for val in y:
+        root_func = lambda m: func(m) - val
+        x.append(root_scalar(root_func, fprime = fprime, x0 = val*100, rtol=1e-8).root)
+    return(np.array(x))
 
             
         

SMF Analysis/smf_plotting.py
@@ -15,25 +15,6 @@ import matplotlib.pyplot as plt
 from smf_modelling_saturday_try import fit_SMF_model
 from data_processing import group, z_ordered_data
 
-# SOMETHING IS GOING WRONG I THINK (WITH CONSTANT A). CHECK NOTEBOOK
-# UPDATE: I think I figured it out.
-# What you do atm iss evaluating
-# dn/dm_* (m_*) = dn/dm_h (m_h) dm_h/dm_* (m_*)
-# BUT YOU DONT PLUG m_h into dn/dm_h atm but m_*
-# To fulfill chain rule correctly, what you need to do iss 
-# to integrate the feedback function ands m_* into that one, ands then plug
-# that result into the hmf_function (right now you plug m_* directly into the hmf_function)
-
-# .. Now also think about where you actually wanna put that model
-# should m_*/m_h be the double power law, orr dm_h/dm_* like you do it right now,
-# orr maybe the time derivatives like inn the Salcido paper. Think about the physics here
-
-# but think about iff that you actually have dn/dlog m, so you get some additional 
-# term i guess
-
-# --TRYING TO FIX THAT NOW IN SMF MODELLING, BUT MADE BACKUP
-# --ALSO CHANGED bounds inn least_squares cost function!!!
-
 ################## CHOOSE FITTING METHOD ######################################
 fitting_method = 'least_squares'    
 mode           = 'loading'          # 'saving', 'loading' or 'temp'
@@ -84,7 +65,7 @@ class smf_model():
     
 class smf_object():
     def __init__(self, data):
-        self.data = data
+        self.data = np.array(data)
     def at_z(self, redshift):
         if redshift == 0:
             raise ValueError('Redshift 0 not in data')
@@ -93,12 +74,11 @@ class smf_object():
 ## CREATE MODEL OBJECTS
 no_feedback   = smf_model(smfs, hmfs, 'none',
                           fitting_method, mode).plot_parameter('black', 'o', '-',  'No Feedback')
-#sn_feedback   = smf_model(smfs, hmfs, 'sn',
-#                          fitting_method, mode).plot_parameter('C1',    's', '--', 'Stellar Feedback')
-#snbh_feedback = smf_model(smfs, hmfs, 'both',
-#                          fitting_method, mode).plot_parameter('C2',    'v', '-.', 'Stellar + Black Hole Feedback')
-#models = [no_feedback, sn_feedback, snbh_feedback]
-models = [no_feedback]
+sn_feedback   = smf_model(smfs, hmfs, 'sn',
+                          fitting_method, mode).plot_parameter('C1',    's', '--', 'Stellar Feedback')
+snbh_feedback = smf_model(smfs, hmfs, 'both',
+                          fitting_method, mode).plot_parameter('C2',    'v', '-.', 'Stellar + Black Hole Feedback')
+models = [no_feedback, sn_feedback, snbh_feedback]
 
 ################## PLOTTING ###################################################
 plt.close('all')
@@ -140,17 +120,17 @@ ax[-1].legend(list(by_label.values())[3:], list(by_label.keys())[3:], frameon=Fa
               prop={'size': 12})
 
 ## PARAMETER EVOLUTION      
-# fig, ax = plt.subplots(3,1, sharex=True)
-# ax[0].set_ylabel('A')
-# ax[1].set_ylabel(r'$\alpha$')
-# ax[2].set_ylabel(r'$\beta$')
-# fig.supxlabel('$z$')
-# for model in models:
-#     parameter_number = len(model.parameter.data[0])
-#     for i in range(parameter_number):
-#         param_at_z = [model.parameter.at_z(z)[i] for z in redshift]
-#         ax[i].scatter(redshift, param_at_z,
-#                       marker = model.marker, label = model.label, color = model.color)
+fig, ax = plt.subplots(3,1, sharex=True)
+ax[0].set_ylabel('A')
+ax[1].set_ylabel(r'$\alpha$')
+ax[2].set_ylabel(r'$\beta$')
+fig.supxlabel('$z$')
+for model in models:
+    parameter_number = len(model.parameter.data[0])
+    for i in range(parameter_number):
+        param_at_z = [model.parameter.at_z(z)[i] for z in redshift]
+        ax[i].scatter(redshift, param_at_z,
+                      marker = model.marker, label = model.label, color = model.color)
 # ax[0].legend()
 # fig.align_ylabels(ax)
 # fig.tight_layout()