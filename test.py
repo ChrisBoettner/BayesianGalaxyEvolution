cba9851ab2bc9dff5a03fc25a181a8cb8a7e7155
Chris Boettner <boettnec@gmail.com>
2022-04-11 17:04:50 +0200

    changed errors again

--- test.py
+++ test.py
@@ -8,10 +8,11 @@ Created on Sun Apr 10 19:27:34 2022
 
 from model.api import run_model
 
-quantity_name = 'mstar'
+quantity_name = 'Muv'
 #feedback_name = 'none'
 
-m = run_model(quantity_name, 'changing', fitting_method = 'least_squares')
+m = run_model(quantity_name, 'changing', data_subset = 'Bouwens2021',
+              fitting_method = 'least_squares')
 
 #m = [run_model(quantity_name, 'none'),
 #     run_model(quantity_name, 'stellar'),
